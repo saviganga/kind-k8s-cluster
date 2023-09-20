776d73b90315967240c4e3474db2419d61529652
saviganga A <saviour@mytruq.com>
2023-09-20 03:40:57 +0100

    create kind cluster - make provisions for inavailability of prerequisite packages

--- setup_cluster/create-kind-cluster.sh
+++ setup_cluster/create-kind-cluster.sh
@@ -10,7 +10,7 @@ else
     exit 1
 fi
 
-# define the cluster name
+# Define the cluster name
 CLUSTER_NAME='ignitedotdev'
 
 KUBECTL="kubectl"
@@ -19,63 +19,76 @@ DOCKER="docker"
 KUBECTL_LATEST_RELEASE_URL="https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/$ARCH/kubectl"
 KUBECTL_CHECKSUM_URL="https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 
+echo "Creating kind cluster..."
 
-
-# check if docker is installed
+# Check if Docker is installed
 dpkg -s $DOCKER &> /dev/null
 
 if [ $? -eq 0 ]; then
     echo "$DOCKER is installed, moving on ..."
 else
-    echo "installing $DOCKER ..."
+    echo "Installing $DOCKER ..."
+
+    # Remove clashing packages
+    sudo apt remove --yes "docker docker-engine docker.io containerd runc" || true
     
-    # remove clashing packages
-    curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge"
+    # Add Docker APT repository
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=$ARCH] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge"
     
     apt-cache policy docker-ce
 
+    sudo apt-get install -y docker-ce
+    sudo systemctl status docker
 
-
-    sudo apt remove --yes "docker docker-engine docker.io containerd runc" || true
-    sudo apt update
-    sudo apt --yes --no-install-recommends install "apt-transport-https ca-certificates"
-    wget --quiet --output-document=- "https://download.docker.com/linux/ubuntu/gpg" | sudo apt-key add -
-    sudo add-apt-repository --yes "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
+    
+    
+    
+    # Update APT cache and install Docker
     sudo apt update
     sudo apt --yes --no-install-recommends install "docker-ce docker-ce-cli containerd.io"
+    
+    # Add the current user to the Docker group
     sudo usermod --append --groups docker "$USER"
+    
+    # Enable Docker service
     sudo systemctl enable docker
-    echo '\nDocker successfully installed\n\n'
+    
+    echo "Docker successfully installed"
    
     sleep 5
 
-    # get docker compose - bonus
-    sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
-    sudo chmod +x /usr/local/bin/docker-compose
-    sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
-    echo "Docker compose successfully installed"
+    # Check if Docker Compose is installed
+    if ! command -v docker-compose &>/dev/null; then
+        echo "Installing Docker Compose ..."
+        sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
+        sudo chmod +x /usr/local/bin/docker-compose
+        sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
+        echo "Docker Compose successfully installed"
+    else
+        echo "Docker Compose is already installed"
+    fi
+fi
 
-echo "Creating kind cluster..."
-# check if kubectl is installed
+# Check if kubectl is installed
 dpkg -s $KUBECTL &> /dev/null
 
 if [ $? -eq 0 ]; then
     echo "$KUBECTL is installed, moving on ..."
 else
-    echo "installing $KUBECTL ..."
+    echo "Installing $KUBECTL ..."
 
-    # download the latest kubectl release for your architecture
+    # Download the latest kubectl release for your architecture
     curl -LO "$KUBECTL_LATEST_RELEASE_URL"
 
-    # install kubectl
+    # Install kubectl
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 fi
 
-# install kind
+# Install kind
 kind create cluster --name $CLUSTER_NAME
 
-# select the config file to be used for this cluster - (the one just created by kind)
+# Select the config file to be used for this cluster (the one just created by kind)
 kubectl config use-context $CLUSTER_NAME
 kubectl cluster-info --context kind-$CLUSTER_NAME
 
