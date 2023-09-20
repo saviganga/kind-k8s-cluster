322e76776a0798a8925a737cbb56044df75f7a74
saviganga A <saviour@mytruq.com>
2023-09-20 02:39:34 +0100

    create kind cluster - make provisions for inavailability of prerequisite packages

--- setup_cluster/create-kind-cluster.sh
+++ setup_cluster/create-kind-cluster.sh
@@ -14,12 +14,43 @@ fi
 CLUSTER_NAME='ignitedotdev'
 
 KUBECTL="kubectl"
+DOCKER="docker"
 
 KUBECTL_LATEST_RELEASE_URL="https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/$ARCH/kubectl"
 KUBECTL_CHECKSUM_URL="https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 
-echo "Creating kind cluster..."
 
+
+# check if docker is installed
+dpkg -s $DOCKER &> /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "$DOCKER is installed, moving on ..."
+else
+    echo "installing $DOCKER ..."
+    
+    # remove clashing packages
+    sudo apt remove --yes "docker docker-engine docker.io containerd runc" || true
+    sudo apt update
+    sudo apt --yes --no-install-recommends install "apt-transport-https ca-certificates"
+    wget --quiet --output-document=- "https://download.docker.com/linux/ubuntu/gpg" | sudo apt-key add -
+    sudo add-apt-repository --yes "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
+    sudo apt update
+    sudo apt --yes --no-install-recommends install "docker-ce docker-ce-cli containerd.io"
+    sudo usermod --append --groups docker "$USER"
+    sudo systemctl enable docker
+    echo '\nDocker successfully installed\n\n'
+   
+    sleep 5
+
+    # get docker compose - bonus
+    echo "Installing docker compose ..."
+    sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
+    echo "Docker compose successfully installed"
+
+echo "Creating kind cluster..."
 # check if kubectl is installed
 dpkg -s $KUBECTL &> /dev/null
 
