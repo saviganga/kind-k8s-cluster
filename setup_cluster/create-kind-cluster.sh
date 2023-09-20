9ce9a3044c9447aa9c24f14f980d7bce9f734e5b
saviganga A <saviour@mytruq.com>
2023-09-20 03:12:23 +0100

    create kind cluster - make provisions for inavailability of prerequisite packages

--- setup_cluster/create-kind-cluster.sh
+++ setup_cluster/create-kind-cluster.sh
@@ -30,13 +30,13 @@ else
     echo "Installing $DOCKER ..."
     
     # Remove clashing packages
-    sudo apt remove --yes "docker docker-engine docker.io containerd runc" || true
+    sudo apt remove --yes docker docker-engine docker.io containerd runc || true
     sudo apt update
-    sudo apt --yes --no-install-recommends install "apt-transport-https ca-certificates"
-    wget --quiet --output-document=- "https://download.docker.com/linux/ubuntu/gpg" | sudo apt-key add -
+    sudo apt --yes --no-install-recommends install apt-transport-https ca-certificates
+    wget --quiet --output-document=- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository --yes "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
     sudo apt update
-    sudo apt --yes --no-install-recommends install "docker-ce docker-ce-cli containerd.io"
+    sudo apt --yes --no-install-recommends install docker-ce docker-ce-cli containerd.io
     sudo usermod --append --groups docker "$USER"
     sudo systemctl enable docker
     echo "Docker successfully installed"
