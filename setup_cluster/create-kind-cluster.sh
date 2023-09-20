c28acef3ad96a0c3177c286d2c545082b583e4ee
saviganga A <saviour@mytruq.com>
2023-09-20 03:32:42 +0100

    create kind cluster - make provisions for inavailability of prerequisite packages

--- setup_cluster/create-kind-cluster.sh
+++ setup_cluster/create-kind-cluster.sh
@@ -10,7 +10,7 @@ else
     exit 1
 fi
 
-# Define the cluster name
+# define the cluster name
 CLUSTER_NAME='ignitedotdev'
 
 KUBECTL="kubectl"
@@ -19,54 +19,63 @@ DOCKER="docker"
 KUBECTL_LATEST_RELEASE_URL="https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/$ARCH/kubectl"
 KUBECTL_CHECKSUM_URL="https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 
-echo "Creating kind cluster..."
 
-# Check if docker is installed
+
+# check if docker is installed
 dpkg -s $DOCKER &> /dev/null
 
 if [ $? -eq 0 ]; then
     echo "$DOCKER is installed, moving on ..."
 else
-    echo "Installing $DOCKER ..."
+    echo "installing $DOCKER ..."
     
-    # Remove clashing packages
-    sudo apt remove --yes docker docker-engine docker.io containerd runc || true
+    # remove clashing packages
+    curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge"
+    
+    apt-cache policy docker-ce
+
+
+
+    sudo apt remove --yes "docker docker-engine docker.io containerd runc" || true
     sudo apt update
-    sudo apt install apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-    sudo apt install docker-ce
-    sudo systemctl status docker
+    sudo apt --yes --no-install-recommends install "apt-transport-https ca-certificates"
+    wget --quiet --output-document=- "https://download.docker.com/linux/ubuntu/gpg" | sudo apt-key add -
+    sudo add-apt-repository --yes "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
+    sudo apt update
+    sudo apt --yes --no-install-recommends install "docker-ce docker-ce-cli containerd.io"
+    sudo usermod --append --groups docker "$USER"
+    sudo systemctl enable docker
+    echo '\nDocker successfully installed\n\n'
    
     sleep 5
 
-    # Get Docker Compose - bonus
-    echo "Installing Docker Compose ..."
+    # get docker compose - bonus
     sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
     sudo chmod +x /usr/local/bin/docker-compose
     sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
-    echo "Docker Compose successfully installed"
-fi
+    echo "Docker compose successfully installed"
 
-# Check if kubectl is installed
+echo "Creating kind cluster..."
+# check if kubectl is installed
 dpkg -s $KUBECTL &> /dev/null
 
 if [ $? -eq 0 ]; then
     echo "$KUBECTL is installed, moving on ..."
 else
-    echo "Installing $KUBECTL ..."
+    echo "installing $KUBECTL ..."
 
-    # Download the latest kubectl release for your architecture
+    # download the latest kubectl release for your architecture
     curl -LO "$KUBECTL_LATEST_RELEASE_URL"
 
-    # Install kubectl
+    # install kubectl
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 fi
 
-# Install kind
+# install kind
 kind create cluster --name $CLUSTER_NAME
 
-# Select the config file to be used for this cluster (the one just created by kind)
+# select the config file to be used for this cluster - (the one just created by kind)
 kubectl config use-context $CLUSTER_NAME
 kubectl cluster-info --context kind-$CLUSTER_NAME
 
